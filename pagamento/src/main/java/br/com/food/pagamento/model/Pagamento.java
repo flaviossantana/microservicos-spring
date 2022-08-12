befb94d2912d31ecc3ec5a8d1493685b616cef3e
flavio.santana <9090flavio@gmail.com>
2022-08-11 22:24:29 -0300

    feat: criando modelo

---
+++ pagamento/src/main/java/br/com/food/pagamento/model/Pagamento.java
@@ -0,0 +1,62 @@
+package br.com.food.pagamento.model;
+
+
+import br.com.food.pagamento.type.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "pagamento")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Positive
+    private BigDecimal valor;
+
+    @NotBlank
+    @Size(max = 100)
+    private String nome;
+
+    @NotBlank
+    @Size(max = 19)
+    private String numero;
+
+    @NotBlank
+    @Size(max = 7)
+    private String expiracao;
+
+    @NotBlank
+    @Size(max = 3, min = 3)
+    private String codigo;
+
+    @NotNull
+    private Long pedidoId;
+
+    @NotNull
+    private Long formaPagamentoId;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+
+
+
+}
---
+++ pagamento/src/main/java/br/com/food/pagamento/type/Status.java
@@ -0,0 +1,8 @@
+package br.com.food.pagamento.type;
+
+public enum Status {
+    CRIADO,
+    CONFIRMADO,
+    CANCELADO
+}
+
