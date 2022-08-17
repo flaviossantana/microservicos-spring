896c73f75b2225d023b047f42c614958344e8e44
flavio.santana <9090flavio@gmail.com>
2022-08-17 08:20:51 -0300

    feat: service

---
+++ pagamento/src/main/java/br/com/food/pagamento/service/PagamentoService.java
@@ -0,0 +1,56 @@
+package br.com.food.pagamento.service;
+
+import br.com.food.pagamento.dto.PagamentoDto;
+import br.com.food.pagamento.model.Pagamento;
+import br.com.food.pagamento.repository.PagamentoRepository;
+import br.com.food.pagamento.type.StatusPagamento;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class PagamentoService {
+
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public Page<PagamentoDto> obterTodos(Pageable paginacao) {
+        return pagamentoRepository
+                .findAll(paginacao)
+                .map(pagamento -> modelMapper.map(pagamento, PagamentoDto.class));
+    }
+
+    public PagamentoDto obterPorId(Long id) {
+        Pagamento pagamento = pagamentoRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public PagamentoDto criar(PagamentoDto dto) {
+        Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
+        pagamento.setStatus(StatusPagamento.CRIADO);
+        pagamento = pagamentoRepository.save(pagamento);
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public PagamentoDto atualizar(Long id, PagamentoDto dto) {
+        Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
+        pagamento.setId(id);
+        pagamento = pagamentoRepository.save(pagamento);
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public void excluir(Long id) {
+        pagamentoRepository.deleteById(id);
+    }
+
+}
+
+
+
