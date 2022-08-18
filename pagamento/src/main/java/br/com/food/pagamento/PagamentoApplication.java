b084cdf4deece614923fd6ffbeb05a2209caf7e5
flavio.santana <9090flavio@gmail.com>
2022-08-17 23:10:30 -0300

    feat: configurações

--- pagamento/src/main/java/br/com/food/pagamento/PagamentoApplication.java
+++ pagamento/src/main/java/br/com/food/pagamento/PagamentoApplication.java
@@ -1,9 +1,7 @@
 package br.com.food.pagamento;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PagamentoApplication {
@@ -11,11 +9,5 @@ public class PagamentoApplication {
     public static void main(String[] args) {
         SpringApplication.run(PagamentoApplication.class, args);
     }
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
-
 
 }
